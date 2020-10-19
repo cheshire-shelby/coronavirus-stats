cef9d66842fe059146c98f21a639e89e907aef03
Thomas Vu <56103689+cheshire-shelby@users.noreply.github.com>
2020-10-19 18:37:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 from tabulate import tabulate
-from PIL import Image, ImageDraw, ImageFont
-import matplotlib.pyplot as plt
-import numpy as np
-from numpy import genfromtxt
-import six
-import csv
-
-
 
 url = 'https://www.worldometers.info/coronavirus/'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
-info = soup.findAll('div', {'class': 'maincounter-number'})
+important_info = soup.findAll('div', {'class': 'maincounter-number'})
+
+
+# url = 'https://www.vnexpress.net/dich-viem-phoi-corona'
+# response = requests.get(url)
+# soup = BeautifulSoup(response.text, "html.parser")
+# important_info = soup.findAll('div')
+# def get_stats():
+#     for c in important_info:
+#         print(c)
+
 
 def get_stats():
     with open('stats.txt', 'w', newline='') as file:
         for i, v in enumerate(('Cases', 'Deaths', 'Recovered')):
-            file.writelines(f'{v}: {info[i].get_text().strip()}\n')
+            file.writelines(f'{v}: {important_info[i].get_text().strip()}\n')
+
 
 def get_table():
-    table = soup.find('table', {'id': 'main_table_countries'})
+    table = soup.find('table', {'id': 'main_table_countries_today'})
     table_body = table.find('tbody')
-    header = 'Country,Total Cases,New Cases,Total Deaths,New Deaths,Active Cases,Total Recovered,Serious, Tot Cases/1M pop'.split(',')
+    header = 'Country,Other,Total\nCases,New\nCases,Total\nDeaths,New\n' \
+             'Deaths,Total\nRecovered,Active\nCases,Serious\nCritical,' \
+             'Tot Cases/\n1M pop,Deaths/\n1M pop,Total\nTests, Test/\n1M pop, Population,a ,a ,a ,a, a'.split(',')
     data = []
     rows = table_body.find_all('tr')
     tabulate.PRESERVE_WHITESPACE = True
@@ -32,7 +37,7 @@ def get_table():
         cols = row.find_all('td')
         cols = [i.text.strip() for i in cols]
         data.append([i for i in cols])
-    df = pd.DataFrame(data, columns = header)
+    df = pd.DataFrame(data, columns=header)
 
     # print(df.iloc[[39]])
     # img = Image.new('RGB', (960, 2000), color='white')
@@ -43,10 +48,11 @@ def get_table():
     # d.text((10, 10), tabulate(df, headers=header, showindex=False), fill= 'black', font = font)
     # img.save('pil_text.png')
 
-    with open('stats.csv', 'w', newline='') as file :
-        export_csv = df.to_csv(r'/home/tommy/PycharmProjects/coronastats/stats.csv',index=False, header=header)
-    with open('corona_table.txt', 'w', newline='') as file1 :
+    # with open('stats.csv', 'w', newline='') as file:
+    #     # export_csv = df.to_csv(r'/home/tommy/PycharmProjects/coronastats/stats.csv', index=False, header=header)
+    with open('corona_table.txt', 'w', newline='') as file1:
         file1.write(tabulate(df, headers=header, showindex=False))
+
 
 def get_news():
     inner_content = soup.find('div', {'id': 'innercontent'})
@@ -58,7 +64,10 @@ def get_news():
             bullets = [x[i].text.strip().rstrip(' [source]') for i in range(len(x))]
             for x in bullets:
                 file.write(f". {x}\n")
-       
+        # for line in file:
+        #     for word in delete_list:
+        #         line = line.replace(word, "")
+        #     file.write(line)
 
 
 get_stats()
